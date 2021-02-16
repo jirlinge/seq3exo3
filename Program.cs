4d4b5544a9e711362eeaf6f33b1ff64dbf696b12
jirlinge <julienirlinger@gmail.com>
2021-02-16 12:08:52 +0100

    derniere correction

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number1;
+            int number2;
+
+            Console.WriteLine("veuillez entrer la premiere valeur");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("veuillez entrer la deuxieme valeur");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+
+            int sum1 = number1 + 33;
+            int increm = ++number2;
+            int result = sum1 / increm;
+
+            Console.WriteLine("le resultat de l'opération est "+result);
+            
+
+            
+            
         }
     }
 }
